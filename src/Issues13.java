7aa8a956dc2cdc59795252801534e4b091ca1fd9
klerikmahno <klerikpavlenko@gmail.ru>
2020-04-21 22:25:35 +0300

    задачка 13

---
+++ src/Issues13.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Issues13 {
+    public static void main(String[] args) {
+        System.out.print("Введите строку #1: ");
+        Scanner console = new Scanner(System.in);
+        String x = console.nextLine();
+        System.out.print("Введите строку #2: ");
+        String y = console.nextLine();
+        if(x.length() == y.length()){
+            System.out.println("Строки равной длины!");
+        }else {
+            System.out.println("Строки разной длины!");
+        }
+    }
+}
