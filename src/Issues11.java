950a13219777140bc8d49a2a76145df65b77cb0f
klerikmahno <klerikpavlenko@gmail.ru>
2020-04-21 22:12:26 +0300

    задачка 11

---
+++ src/Issues11.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Issues11 {
+    public static void main(String[] args) {
+        System.out.print("Введите строку: ");
+        Scanner console = new Scanner(System.in);
+        String x = console.nextLine();
+        System.out.print(x);
+    }
+}
