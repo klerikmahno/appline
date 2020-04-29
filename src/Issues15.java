409f587003e8151375edd1ea6f956b0833f4d0f9
klerikmahno <klerikpavlenko@gmail.ru>
2020-04-29 22:59:22 +0300

    задачка 15

---
+++ src/Issues15.java
@@ -0,0 +1,24 @@
+import javax.jws.soap.SOAPBinding;
+import java.util.*;
+
+public class Issues15 {
+    public static void main(String[] args) {
+        System.out.println("Введите масcив: ");
+        Scanner console = new Scanner(System.in);
+        String[] a = console.nextLine().split(" ");
+        int[] b = new int[a.length];
+        for (int i = 0; i < b.length; i++) {
+            b[i] = Integer.parseInt(a[i]);
+        }
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < b.length-1; j++) {
+                if(b[j+1]<b[j]){
+                    int temp = b[j+1];
+                    b[j+1] = b[j];
+                    b[j] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(b));
+    }
+}
